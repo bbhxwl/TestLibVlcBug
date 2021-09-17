85b293f9a2d20c17057fcae6c7709d5f4b07045c
bbhxwl <470138890@qq.com>
2021-09-17 09:22:26 +0800

    添加项目文件。

---
+++ TestLibVlcBug/TestLibVlcBug/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using LibVLCSharp.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestLibVlcBug
+{
+    public partial class MainPage : ContentPage
+    {
+
+        LibVLC _libVLC;
+        public MainPage()
+        {
+            InitializeComponent();
+            Core.Initialize();
+            _libVLC = new LibVLC(enableDebugLogs: false);
+            var _mediaPlayer = new MediaPlayer(_libVLC);
+            video.MediaPlayer = _mediaPlayer;
+            //The third to fourth seconds will be not smooth
+            //第三到四秒会出现不流畅
+            _mediaPlayer.Play(new Media(_libVLC, "https://bj.bcebos.com/kpy-organizations/U12760068/videos/2021083110332897820349837654.mp4", FromType.FromLocation));
+
+        }
+    }
+}
